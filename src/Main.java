8a57d9fb582f37228f8f5e249dc2102eb7b93126
Vitor-Tx <49378292+Vitor-Tx@users.noreply.github.com>
2019-12-12 14:37:03 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import model.composite.Sala;
-import model.factory.impl.LabirintoFactory;
+import view.Jogo;
 
 public class Main {
     public static void main(String args[]) throws InterruptedException {
-        //Jogo j = new Jogo();
-        //j.iniciar(j);
+        Jogo j = new Jogo();
+        j.iniciar(j);
 
-        Sala labirinto = LabirintoFactory.criaFase();
-        labirinto.entrar();
+        //Sala labirinto = LabirintoFactory.criaFase();
+        //labirinto.entrar();
     }
 }
